cbc787411000921015249ce01cb65f0452f0b44f
coelhos-gabi <87398488+coelhos-gabi@users.noreply.github.com>
2021-10-17 20:18:34 -0300

    Delete Django directory

--- Django/primeiro_projeto_django/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views #ponto = este mesmo diretório
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('<str:name>', views.greet, name='greet'),
-    path("gabi", views.gabi, name="gabi"),
-    path('camila', views.camila, name='camila')
-]
